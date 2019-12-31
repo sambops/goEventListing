ebbdea4443e66b39c36e42b5d5931162fa472017
birukbelay <belay.bura@gmail.com>
2019-12-31 08:15:59 +0300

    tags added

---
+++ entity/tags.go
@@ -0,0 +1,18 @@
+package entity
+
+//Tag there are multiple tags for the user to choose from
+type Tag struct {
+	ID         int
+	Name       string
+	categoryID int
+}
+
+//User_tags is used when a user register to tags
+// type User_tags struct {
+// 	UserID, TagID int
+// }
+
+// Event_Tag is used when an event is posted
+// type Event_Tag struct {
+// 	Event_ID, TagID int
+// }
