142e1d1e1d67478a21b40d0da877f15809d99e23
lazaros <ej.dalvic@gmail.com>
2020-01-04 01:44:48 +0300

    check ::#	modified:   event/services.go

--- event/services.go
+++ event/services.go
@@ -7,8 +7,18 @@ import (
 //USECASE
 //this is our event usescase(has (interface)abstract classes that outer layers can use)
 type EventService interface {
-	Events() ([]entity.Event, error)
-	AddEvent(event entity.Event) error
-	EditEvent(event entity.Event) error
+	Events() ([]entity.Event, error) //get list of events
+	Event(id int) (entity.Event, error)
+	UpcomingEvents() ([]entity.Event, error)
+	
+	Post(event entity.Event) error
+	addTag(id []int)                           error//?? how do we add multiple tags
+	UpdateEvent(event entity.Event) error
+	
 	DeleteEvent(id int) error
+	rate(EventID, UserID, rating int)  error
+	setRating(eventID int) error //this is done in the back
+	
+	getMyRating(UID, EventID int)    int	
+	notify(eventID int, tagsID []int)          error
 }
