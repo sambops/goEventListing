fbf6559e895e78d3929e620aae602a9869041f41
bura <belay.bura@gmail.com>
2020-01-07 21:38:47 +0300

    i have finished the repository code and editted other stuff

--- review/repository.go
+++ review/repository.go
@@ -1,18 +1,17 @@
-package reviews
+package review
 
 import "github.com/birukbelay/Aprojects/goEventListing/entity"
 
-// ReviewRepository repository
+// ReviewRepository specifies application review related operations
 type ReviewRepository interface {
-	Reviews() ([]entity.Review, []error)
-	
-	MakeReview(review *entity.Review, comment entity.Comment) error
+	Reviews() ([]entity.Review, error)
+	Review(id int) (entity.Review, error)
+	MakeReview(r entity.Review) error
+	SetRating(Eid int) error
+	UpdateReview(r entity.Review) error
+	DeleteReview(id int) error
+	GetMyReviews(id int) ([]entity.Review, error)
 	setRating(eventID int) error //this is done in the back when a event is rated
-
-	deleteReview(id int) error
-	updateReview(id int) error
-
-	getMyReviews(user *entity.User) ([]entity.Review, []error) // list of reviews a user have given
 
 	// getMyRating(UID, EventID int) int
 	// deleteComment(id int) error
--- review/repository/psql_review.go
+++ review/repository/psql_review.go
@@ -19,7 +19,9 @@ func NewReviewRepoImpl(Con *sql.DB) *ReviewRepoImpl {
 
 // Reviews returns all Reviews from the database
 func (rri *ReviewRepoImpl) Reviews() ([]entity.Review, error) {
-	rows, err := rri.conn.Query("SELECT * FROM review;")
+
+	query := "SELECT * FROM review"
+	rows, err := rri.conn.Query(query)
 	if err != nil {
 		return nil, errors.New("Could not query the database")
 	}
@@ -43,7 +45,8 @@ func (rri *ReviewRepoImpl) Reviews() ([]entity.Review, error) {
 // Review returns a Review with a given id
 func (rri *ReviewRepoImpl) Review(id int) (entity.Review, error) {
 
-	row := rri.conn.QueryRow("SELECT * FROM review WHERE id = $1", id)
+	query := "SELECT * FROM review WHERE id = $1"
+	row := rri.conn.QueryRow(query, id)
 
 	r := entity.Review{}
 
@@ -58,7 +61,8 @@ func (rri *ReviewRepoImpl) Review(id int) (entity.Review, error) {
 // MakeReview stores new review information to database
 func (rri *ReviewRepoImpl) MakeReview(r entity.Review) error {
 
-	_, err := rri.conn.Exec("INSERT INTO review (rating,event_id,user_id,message) values($1, $2, $3, $4)", r.Rating, r.EventID, r.UserID, r.Message)
+	query := "INSERT INTO review (rating,event_id,user_id,message) values($1, $2, $3, $4)"
+	_, err := rri.conn.Exec(query, r.Rating, r.EventID, r.UserID, r.Message)
 	if err != nil {
 		return errors.New("reviewing has failed")
 	}
@@ -68,8 +72,8 @@ func (rri *ReviewRepoImpl) MakeReview(r entity.Review) error {
 
 // UpdateReview updates a given object with a new data
 func (rri *ReviewRepoImpl) UpdateReview(r entity.Review) error {
-
-	_, err := rri.conn.Exec("UPDATE review SET rating=$1,event_id=$2, user_id=$3, message=$4 WHERE id=$5", r.Rating, r.EventID, r.UserID, r.Message, r.ID)
+	query := "UPDATE review SET rating=$1,event_id=$2, user_id=$3, message=$4 WHERE id=$5"
+	_, err := rri.conn.Exec(query, r.Rating, r.EventID, r.UserID, r.Message, r.ID)
 
 	if err != nil {
 		return errors.New("updating has failed")
@@ -80,8 +84,8 @@ func (rri *ReviewRepoImpl) UpdateReview(r entity.Review) error {
 
 // DeleteReview removes a review from a database by its id
 func (rri *ReviewRepoImpl) DeleteReview(id int) error {
-
-	_, err := rri.conn.Exec("DELETE FROM review WHERE id=$1", id)
+	query := "DELETE FROM review WHERE id=$1"
+	_, err := rri.conn.Exec(query, id)
 
 	if err != nil {
 		return errors.New("Delete has failed")
@@ -93,7 +97,8 @@ func (rri *ReviewRepoImpl) DeleteReview(id int) error {
 // GetMyReviews returns the reviews of a single user
 func (rri *ReviewRepoImpl) GetMyReviews(id int) ([]entity.Review, error) {
 
-	rows, err := rri.conn.Query("SELECT * FROM review WHERE User_id = $1", id)
+	query := "SELECT * FROM review WHERE User_id = $1";
+	rows, err := rri.conn.Query(query, id)
 
 	if err != nil {
 		return nil, errors.New("Could not query the database")
@@ -108,6 +113,7 @@ func (rri *ReviewRepoImpl) GetMyReviews(id int) ([]entity.Review, error) {
 		if err != nil {
 			return nil, err
 		}
+
 		rvws = append(rvws, review)
 	}
 
@@ -117,7 +123,8 @@ func (rri *ReviewRepoImpl) GetMyReviews(id int) ([]entity.Review, error) {
 //SetRating sets the average rating of an event after every reviews
 func (rri *ReviewRepoImpl) SetRating(Eid int) error {
 
-	row := rri.conn.QueryRow("SELECT AVG(rating) FROM review WHERE Event_id = $1", Eid)
+	query :="SELECT AVG(rating) FROM review WHERE Event_id = $1"
+	row := rri.conn.QueryRow(query, Eid)
 
 	var rating float32
 	err := row.Scan(&rating)
@@ -125,9 +132,9 @@ func (rri *ReviewRepoImpl) SetRating(Eid int) error {
 	if err != nil {
 		return errors.New("Could not make average in the database")
 	}
-	_, err := rri.conn.Exec("UPDATE events SET rating=$1 WHERE id=$1", rating, Eid)
+	_, er := rri.conn.Exec("UPDATE events SET rating=$1 WHERE id=$1", rating, Eid)
 
-	if err != nil {
+	if er != nil {
 		return errors.New("setting new rating has failed")
 	}
 	return nil
