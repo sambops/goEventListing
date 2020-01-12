1a5a9b229077c8e50d7e5fa35afd74198cd51835
sami <sambapz20@gmail.com>
2020-01-12 12:05:13 -0800

    review-handler james cross check

---
+++ API/delivery/http/handler/reviewHandler.go
@@ -0,0 +1,225 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/goEventListing/API/entity"
+	"github.com/goEventListing/API/review"
+	"github.com/julienschmidt/httprouter"
+)
+
+//ReviewHandler ...
+type ReviewHandler struct {
+	revserv review.ReviewService
+}
+
+// NewReviewHandler ...
+func NewReviewHandler(rs review.ReviewService) *ReviewHandler {
+	return &ReviewHandler{revserv: rs}
+}
+
+//Reviews ... handles GET reviews
+func (rh *ReviewHandler) Reviews(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	rvws, err := rh.revserv.Reviews()
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	output, errr := json.MarshalIndent(rvws, "", "\t\t")
+	if errr != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+
+}
+
+//Review ... handles GET /event/Review/:id request
+func (rh *ReviewHandler) Review(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	id, err := strconv.Atoi(ps.ByName("id"))
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	rvw, errs := rh.revserv.Review(uint(id))
+	if errs != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	output, err := json.MarshalIndent(rvw, "", "\t\t")
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+
+}
+
+//MakeReview ...  handles Post /event/make request
+func (rh *ReviewHandler) MakeReview(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	l := req.ContentLength
+	body := make([]byte, l)
+	req.Body.Read(body)
+	review := &entity.Review{}
+
+	err := json.Unmarshal(body, review)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	review, errs := rh.revserv.MakeReview(review)
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	p := fmt.Sprintf("/event/review/%d", review.ID)
+	w.Header().Set("Location", p)
+	w.WriteHeader(http.StatusCreated)
+	return
+}
+
+//EventReviews ...  handles GET /event/reviews/:id request
+func (rh *ReviewHandler) EventReviews(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+
+	id, err := strconv.Atoi(ps.ByName("id"))
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	rvws, errs := rh.revserv.EventReviews(uint(id))
+	if errs != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	output, err := json.MarshalIndent(rvws, "", "\t\t")
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+
+}
+
+//GetMyReviews ... handles GET /user/reviews
+func (rh *ReviewHandler) GetMyReviews(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	id, err := strconv.Atoi(ps.ByName("id"))
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	rvws, errs := rh.revserv.GetMyReviews(uint(id))
+
+	if len(errs) > 0 {
+
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	fmt.Println("rvws,,---", rvws) //prints the reviews
+	output, err := json.MarshalIndent(rvws, "", "\t\t")
+	fmt.Println("output---", output)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+
+}
+
+//EditReview handles PUT /event/review/:id request
+func (rh *ReviewHandler) EditReview(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	id, err := strconv.Atoi(ps.ByName("id"))
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	review, errs := rh.revserv.Review(uint(id))
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	l := r.ContentLength
+
+	body := make([]byte, l)
+
+	r.Body.Read(body)
+
+	json.Unmarshal(body, &review)
+
+	review, errs = rh.revserv.UpdateReview(review)
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	output, err := json.MarshalIndent(review, "", "\t\t")
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+}
+
+// DeleteReview handles DELETE /event/review/:id request
+func (rh *ReviewHandler) DeleteReview(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	id, err := strconv.Atoi(ps.ByName("id"))
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	_, errs := rh.revserv.DeleteReview(uint(id))
+
+	if len(errs) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNoContent)
+	return
+}
